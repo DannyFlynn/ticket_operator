23c41fe1ce930e373a86efbdf05ece90b32d1284
Danny Flynn <flynny386@gmail.com>
2024-08-21 14:53:34 +0100

    moved an export away from page file into own file employees as page.file has one export

---
+++ employees.ts
@@ -0,0 +1,38 @@
+export const employees = [
+    {
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        phone: "+44 7911 123456",
+        dob: "1985-02-15",
+        startDate: "2020-01-10"
+    },
+    {
+        name: "Jane Smith",
+        email: "janesmith@hotmail.com",
+        phone: "+44 7911 234567",
+        dob: "1990-07-22",
+        startDate: "2018-04-23"
+    },
+    {
+        name: "Michael Brown",
+        email: "michaelbrown@gmail.com",
+        phone: "+44 7911 345678",
+        dob: "1988-11-10",
+        startDate: "2019-09-15"
+    },
+    {
+        name: "Emily Davis",
+        email: "emilydavis@hotmail.com",
+        phone: "+44 7911 456789",
+        dob: "1992-03-30",
+        startDate: "2021-05-18"
+    },
+    {
+        name: "William Johnson",
+        email: "williamjohnson@gmail.com",
+        phone: "+44 7911 567890",
+        dob: "1983-08-05",
+        startDate: "2017-11-30"
+    }
+  ];
+  
