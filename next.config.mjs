bd7c8be74c6741c7087f892033abe93782a4f158
Danny Flynn <flynny386@gmail.com>
2024-08-22 08:58:25 +0100

    added cors policy in config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers(){
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*"},
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" }
+                ]
+            }
+        ]
+    }
+};
 
 export default nextConfig;
